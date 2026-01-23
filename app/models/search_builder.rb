74d7e6ddc442a19691d321448bea8ef42bf04b01
sarahman-tcd <sarahman@tcd.ie>
2026-01-23 11:46:50 +0000

    Advanced Search Implementation- Fix- "Any" with or without DateFilter

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
-class SearchBuilder < Blacklight::SearchBuilder
+class SearchBuilder < Hyrax::CatalogSearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   # Add a filter query to restrict the search to documents the current user has access to
   include Hydra::AccessControlsEnforcement
   include Hyrax::SearchFilters
 
+  self.default_processor_chain += [:force_lucene_parser]
 
-  ##
-  # @example Adding a new step to the processor chain
-  #   self.default_processor_chain += [:add_custom_data_to_query]
-  #
-  #   def add_custom_data_to_query(solr_parameters)
-  #     solr_parameters[:custom] = blacklight_params[:user_value]
-  #   end
+  def force_lucene_parser(solr_parameters)
+    if blacklight_params[:advanced_search] == 'true'
+      solr_parameters[:defType] = 'lucene'
+    end
+  end
 end
